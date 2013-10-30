7c81a8c7a4e10b3110d0ccc45d95bb7e8dddacb5
LiuYa <liuya@LiuYas-MacBook-Air.local>
2013-10-30 17:38:27 -0400

    test

---
+++ Test.java
@@ -0,0 +1,30 @@
+public class Test{
+
+  public static void main(String[] args){
+
+    System.out.println("********************Start********************");
+    System.out.println("*********************************************\n\n");
+
+    /*
+     * add code
+     */
+    
+    
+
+
+
+
+
+    System.out.println("\n\n*********************************************");
+    System.out.println("*********************End*********************");
+  }
+
+
+  /*
+   * add function
+   */
+
+
+
+
+}
